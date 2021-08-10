f6e03db7dcb28cf18320dded7c029a4a0ce83f30
CodeGorgeous <2460481461@qq.com>
2021-08-10 22:36:48 +0800

    v1.2.0

--- plugin/markdown-html-transform/index.js
+++ plugin/markdown-html-transform/index.js
@@ -4,19 +4,19 @@ const { resolve } = require('path')
 const marked = require('marked')
 
 class MarkdownHtmlTransform{
-    constructor({ path, filename = 'index.html' }) {
+    constructor({ path, filename = 'index'}) {
         // 判断是否传入path
         if (!path) {
             throw new Error('Please pass in the markdown file path you want to convert')
         }
         this.path = path;
-        this.filename = filename;
+        this.filename = filename + '.html';
     }
     apply(compiler) {
         compiler.hooks.emit.tap('markdown-html-transform', (compilation) => {
             const fileResources = compilation.assets
             // 读取模版的内容
-            const temeplateHtml = readFileSync(resolve(__dirname, '../../', 'target' ,'temeplate.html'), 'utf8')
+            const temeplateHtml = readFileSync(resolve(__dirname, 'temeplate.html'), 'utf8')
             // 读取target目标markdown的内容
             const targetMarkdown = readFileSync(this.path, 'utf8')
             const renderer = {
@@ -27,8 +27,6 @@ class MarkdownHtmlTransform{
             };
             marked.use({ renderer })
             const markdownHtml = marked(targetMarkdown)
-            console.log(markdownHtml)
-            // return
             // 查询到模版中<!-- content -->这个注释的位置，将markdown转换后的html放入这里
             const result = temeplateHtml.replace('<!-- content -->', markdownHtml)
             // 放入temeplateHtml中并形成一个文件
